775bcf375f5124e083f5464e4c5716277cc88c22
Josue Aguilera Santos <josue.aguilera@sivah.net>
2021-06-08 17:47:41 -0400

    Created project

---
+++ src/main/java/com/infybuzz/report/FirstReport.java
@@ -0,0 +1,20 @@
+package com.infybuzz.report;
+
+public class FirstReport {
+
+	public static void main(String[] args) {
+		
+		try {
+            System.out.println("");
+            System.out.println("******************************************");
+            System.out.println("* Welcome to JasperSoft Project Tutorial *");
+            System.out.println("******************************************");
+            System.out.println("");
+
+
+        } catch(Exception e) {
+			System.out.println("Exception while creating report");
+		}
+	}
+
+}
