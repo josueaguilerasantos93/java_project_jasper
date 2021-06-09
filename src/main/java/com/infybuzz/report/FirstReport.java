524abbdbd303e18845425c9513465ede32949762
Josue Aguilera Santos <josue.aguilera@sivah.net>
2021-06-09 04:03:50 -0400

    Created the Student Report with Ordered Table and Pie Chart

--- src/main/java/com/infybuzz/report/FirstReport.java
+++ src/main/java/com/infybuzz/report/FirstReport.java
@@ -27,28 +27,46 @@ public class FirstReport {
     public static final SimpleDateFormat fullDateTimeFormatter = new SimpleDateFormat(FULL_DATETIME_FORMAT, new Locale("en", "US"));
     public static final ObjectMapper MAPPER = new ObjectMapper();
 
-	public static void main(String[] args) {
-        String templatesLocation = "D:\\DOCUMENTACION & TUTORIALES\\Curso Udemy_Build Reports with JasperReports Java and Spring Boot" +
-                "\\course_projects\\java_project_jasper\\src\\main\\resources\\templates\\";
-        String reportsLocation = "D:\\DOCUMENTACION & TUTORIALES\\Curso Udemy_Build Reports with JasperReports Java and Spring Boot\\course_projects\\Exported Reports\\";
-        String firstReportName = "FirstReport";
-        String pdfExtension = "pdf";
-        String templateName = "FirstReport.jrxml";
+    public static final String TEMPLATES_LOCATION = "D:\\DOCUMENTACION & TUTORIALES\\Curso Udemy_Build Reports with JasperReports Java and Spring Boot" +
+            "\\course_projects\\java_project_jasper\\src\\main\\resources\\templates\\";
 
-		try {
-            System.out.println();
-            System.out.println("******************************************");
-            System.out.println("* Welcome to JasperSoft Project Tutorial *");
-            System.out.println("******************************************");
-            System.out.println();
-            System.out.println(String.format("* Generating %s document:", firstReportName));
+    public static final String REPORTS_LOCATION = "D:\\DOCUMENTACION & TUTORIALES\\Curso Udemy_Build Reports with JasperReports Java and Spring Boot\\course_projects\\Exported Reports\\";
+
+    public static final String pdfExtension = "pdf";
+    public static final String jrxmlExtension = "jrxml";
+    public static final String firstReportName = "FirstReport";
+    public static final String studentReportName = "Student";
+
+	public static void main(String[] args) {
+        System.out.println();
+        System.out.println("******************************************");
+        System.out.println("* Welcome to JasperSoft Project Tutorial *");
+        System.out.println("******************************************");
+        System.out.println();
+
+        // Create FirstReport
+        generateFirstReport();
+        System.out.println();
+        System.out.println();
+
+        // Create Student Report
+        generateStudentReport();
+
+	}
+
+    private static void generateFirstReport() {
+	    String reportName = firstReportName;
+        try {
+            System.out.println(String.format("* Generating %s document:", reportName));
+
+            // build parameters
             Map<String, Object> parameters = new HashMap<String, Object>();
             parameters.put("studentName", "Josue Aguilera Santos");
             parameters.put("studentAge", 27);
             parameters.put("studentBirthDate", fullDateFormatter.parse("1993-09-26"));
-
             System.out.println("Parameters: " + MAPPER.writeValueAsString(parameters));
 
+            // build fields
             Student student1 = new Student(1, "Josue", "Aguilera Santos", "Patrocinio", "La Habana");
             Student student2 = new Student(2, "Maria", "Gimenez", "Jose Marti", "La Habana");
             Student student3 = new Student(3, "Juan", "Gonzalez", "Alameda", "La Habana");
@@ -60,13 +78,76 @@ public class FirstReport {
             studentList.add(student4);
             System.out.println("Fields: " + MAPPER.writeValueAsString(studentList));
 
+            // Creating datasource
             System.out.print("Creating DataSource... ");
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(studentList);
             System.out.println("DONE");
-            System.out.print("Compiling Report... ");
-            JasperReport jasperReport = JasperCompileManager.compileReport(templatesLocation + templateName);
+
+            generateReport(reportName, parameters, dataSource);
+
+        } catch(Exception e) {
+            System.out.println("Exception while creating report: " + reportName);
+            System.out.println(e.getClass().getSimpleName() + ": "+ e.getLocalizedMessage());
+        }
+    }
+
+    private static void generateStudentReport() {
+        String reportName = studentReportName;
+        try {
+            System.out.println(String.format("* Generating %s document:", reportName));
+
+            // build parameters
+            Map<String, Object> parameters = new HashMap<String, Object>();
+            parameters.put("studentName", "Josue Aguilera Santos");
+
+            // build fields
+            List<Subject> subjectList = new ArrayList<Subject>();
+            subjectList.add(new Subject("Java", 80));
+            subjectList.add(new Subject("MySQL", 70));
+            subjectList.add(new Subject("PHP", 50));
+            subjectList.add(new Subject("MongoDB", 40));
+            subjectList.add(new Subject("C++", 60));
+            JRBeanCollectionDataSource tableDataSource = new JRBeanCollectionDataSource(subjectList);
+
+            // chartDataSource
+            JRBeanCollectionDataSource chartDataSource = new JRBeanCollectionDataSource(subjectList);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("tableData", tableDataSource);
+            map.put("chartDataSource", chartDataSource);
+
+            List<Map<String, ?>> mapList = new ArrayList<Map<String, ?>>();
+            mapList.add(map);
+
+            // Creating datasource
+            System.out.print("Creating DataSource... ");
+            JRMapCollectionDataSource dataSource = new JRMapCollectionDataSource(mapList);
             System.out.println("DONE");
 
+            generateReport(reportName, parameters, dataSource);
+        } catch(Exception e) {
+            System.out.println("Exception while creating report: " + reportName);
+            System.out.println(e.getClass().getSimpleName() + ": "+ e.getLocalizedMessage());
+        }
+    }
+
+	private static void generateReport(String reportName, Map<String, Object> parameters, JRRewindableDataSource dataSource) throws Exception{
+        System.out.print("Compiling Report... ");
+        JasperReport jasperReport = JasperCompileManager.compileReport(TEMPLATES_LOCATION + reportName + "." + jrxmlExtension);
+        System.out.println("DONE");
+        runtimeModifications(reportName, jasperReport);
+        System.out.print("Filling Report... ");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        System.out.println("DONE");
+        String fileName = getFileName(reportName, pdfExtension);
+        System.out.print("Exporting Report... ");
+        JasperExportManager.exportReportToPdfFile(jasperPrint,REPORTS_LOCATION + fileName);
+        System.out.println("DONE");
+        System.out.println("Report successfully exported to: " + REPORTS_LOCATION + fileName);
+    }
+
+    private static void runtimeModifications(String reportName, JasperReport jasperReport){
+	    if (reportName.equals(firstReportName)){
             // Modifications at runtime
             // Can modify any property like: location, alignment, size, backcolor,
             JRTextField studentNameTextField = (JRTextField) jasperReport.getPageHeader().getElementByKey("studentName");
@@ -77,22 +158,9 @@ public class FirstReport {
 
             JRTextField studentBirthDateTextField = (JRTextField) jasperReport.getPageHeader().getElementByKey("studentBirthDate");
             studentBirthDateTextField.setForecolor(Color.green);
+        }
+    }
 
-            System.out.print("Filling Report... ");
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-            System.out.println("DONE");
-
-            String fileName = getFileName(firstReportName, pdfExtension);
-            System.out.print("Exporting Report... ");
-            JasperExportManager.exportReportToPdfFile(jasperPrint,reportsLocation + fileName);
-            System.out.println("DONE");
-            System.out.println("Report successfully exported to: " + reportsLocation + fileName);
-
-        } catch(Exception e) {
-			System.out.println("Exception while creating report: " + firstReportName);
-            System.out.println(e.getClass().getSimpleName() + ": "+ e.getLocalizedMessage());
-		}
-	}
 
 	private static String getFormattedCurrentDatetime(){
 	    return fileDateFormatter.format(new Date());
@@ -102,9 +170,5 @@ public class FirstReport {
 //        return fileName + "_" + getFormattedCurrentDatetime() + "." + fileExtension;
         return fileName + "." + fileExtension;
     }
-
-    /**
-     * JRMapCollectionDataSource
-    * */
 
 }
